a1562be698edc0d7dc4212059d2e83a1650d89f1
Qudus Lawal <lawalqudus980@gmail.com>
2023-12-13 14:01:28 +0100

    Rename file paths

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+import reportWebVitals from './test/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
   </React.StrictMode>
 );
 
