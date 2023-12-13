8ae68e181d9a25a75c86674799bb143c908684d8
Qudus Lawal <lawalqudus980@gmail.com>
2023-12-13 16:15:43 +0100

    Add changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import logo from '../assets/image/bolt-logo.webp'
+import logo from '../assets/image/bolt.svg'
+import ng from '../assets/image/ng.svg'
+import down from '../assets/image/down.svg'
+import Button from './shared/Button';
 const Navbar = () => {
-    return(
-        <>
-            <div>
-                <img src={logo} alt="Bolt logo" />
-            </div>
-        </>
-    )
+    return (
+			<>
+				<div className="flex bg-slate-500">
+					<img src={logo} alt="Bolt logo" />
+					<div className="items-center">
+						<Button logo={ng} text="Nigeria(EN)" image={down} />
+					</div>
+				</div>
+			</>
+		);
 }
 
 export default Navbar;
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,9 +1,17 @@
-const Button = ({value}) => {
+const Button = ({logo, text,image }) => {
     return (
-        <>
-            <input type='submit' value={value} />
-        </>
-    )
+			<>
+				{/* <input type='submit' value={value} /> */}
+				<button
+					type="submit"
+					className="flex items-center gap-2 font-normal text-sm h-full "
+				>
+					<img src={logo} alt="ng" className="w-[20px] border rounded-full" />
+					{text}
+					<img src={image} alt="chevron-down" className="w-[25px] p-0" />
+				</button>
+			</>
+		);
 }
 
 export default Button;
