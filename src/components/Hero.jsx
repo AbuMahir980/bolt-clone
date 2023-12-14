771f60d0dad0298cbb16ddea6fbe826df19e5a5e
Qudus Lawal <lawalqudus980@gmail.com>
2023-12-14 18:17:58 +0100

    Add bg-img + Styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
 // import hero from '../assets/image/hero.webp'
+
 const Hero = () => {
-    return (
-			<>
-				<div className=" bg-[url('../assets/image/hero.webp')] h-screen">
+	return (
+		<>
+			<div>
+				<div className=" bg-[url('../assets/image/hero.webp')] h-screen bg-cover"></div>
+				<div className="w-full h-full absolute inset-0 bg-gradient-to-t md:bg-gradient-to-br from-[rgba(0,0,0,0.64)] to-[rgba(0,0,0,0)]"></div>
+			</div>
+		</>
+	);
+};
 
-                </div>
-			</>
-		)
-}
-
-export default Hero
+export default Hero;
